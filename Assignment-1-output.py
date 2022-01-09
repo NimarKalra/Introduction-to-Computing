3b373693614fadaf9711ff588960371f319e0507
Nimar Kalra <96583894+NimarKalra@users.noreply.github.com>
2022-01-09 15:35:20 +0530

    Add files via upload

---
+++ Assignment-1-output.py
@@ -0,0 +1,36 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============================================================== RESTART: /Users/nimarkalra/Desktop/Python Assignment-1/Assignment-1.py ===============================================================
+Question 1
+
+Enter 1st no.: 1
+Enter 2nd no.: 2
+Enter 3rd no.: 3
+Average = 2.0
+Question 2
+
+Enter your Gross Income $1000000
+Enter no. of Dependents: 4
+Your tax = $195600.0
+Question 3
+
+SID: 21105075
+Name: Nimar
+Gender(F/M/U): M
+Course Name: Computing
+CGPA: 8
+Student: [21105075, 'Nimar', 'M', 'Computing', 8.0]
+Question 4
+
+Enter marks of students: 10
+Enter marks of students: 20
+Enter marks of students: 30
+Enter marks of students: 40
+Enter marks of students: 50
+Sorted Marks: ['10', '20', '30', '40', '50']
+Question 5
+Part a
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+Part b
+['Red', 'Green', 'White', 'Purple', 'Yellow']
