d814296fd3bda8c4dc7038ecaa4288d7af91715c
Nimar Kalra <96583894+NimarKalra@users.noreply.github.com>
2022-01-19 13:09:37 +0530

    Add files via upload

---
+++ Output_Asgn2.py
@@ -0,0 +1,48 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========================================================================= RESTART: /Users/nimarkalra/Desktop/Python/Asgn2.py =========================================================================
+Question1
+
+Part a 
+Length of Input String = 35
+
+Part b
+egaugnal evitisnes esac a si nohtyP
+
+Part c
+a case sensitive
+
+Part d
+Python is object oriented language
+
+Part e
+10
+
+Part f
+Pythonisacasesensitivelanguage
+Question 2
+
+Hey, Nimar Kalra here! 
+My SID is 21105075 
+I am from ECE and my CGPA is 9
+Question 3
+ a&b :  8
+ a|b :  58
+ a^b :  50
+a<<2 :  224 	b<<2 : 40
+a>>2 :  14 	b>>4 : 0
+Question 4
+Enter 1st no. : 3
+Enter 2nd no. : 4
+Enter 3rd no. : 5
+Highest no. = 5
+Qustion 5
+Enter input string: my name
+Yes
+Question 6
+
+Enter 1st length : 6
+Enter 2nd length : 8
+Enter 3rd length : 10
+Yes
